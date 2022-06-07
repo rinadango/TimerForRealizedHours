de0fa705d54ca35aede874175d751c18e584ce98
rinadango <ipaulachan@gmail.com>
2022-06-07 15:58:02 +0300

    Refractored the code

--- realized_hours.py
+++ realized_hours.py
@@ -4,58 +4,64 @@ from datetime import datetime
 from csv import writer
 import sys
 
-# Does the CSV file exist??
-file_csv = input("Create a new CSV file? 1 - Yes, 0 - No: ")
-if file_csv == '1':
-    columns = ['DATE','Project name', 'Time', 'Unit']
-    df = pd.DataFrame(columns = columns)  
-    df.to_csv('Realized_hours.csv', index=False)
-elif file_csv == '0':
-    pass
+def realized_h():
+    # Does the CSV file exist??
+    file_csv = input("Create a new CSV file? 1 - Yes, 0 - No: ")
+    if file_csv == '1':
+        columns = ['DATE','Project name', 'Time', 'Unit']
+        df = pd.DataFrame(columns = columns)  
+        df.to_csv('Realized_hours.csv', index=False)
+    elif file_csv == '0':
+        pass
 
-shortDate = datetime.today().strftime('%Y-%m-%d')
-project_name = input("ENTER project name: ")
+    shortDate = datetime.today().strftime('%Y-%m-%d')
+    project_name = input("ENTER project name: ")
 
-while True:
 
-    input("Press ENTER to continue and ENTER again to exit the TIMER")
-    start_time=time.time()
-    print("Timer is running")
+    while True:
 
-    data = [shortDate,project_name]
+        input("Press ENTER to continue and ENTER again to exit the TIMER")
+        start_time=time.time()
+        print("Timer is running")
 
-    if input() == '':
-        print("Timer has stopped")
-        end_time=time.time()
+        data = [shortDate,project_name]
 
-        time_sec = round(end_time-start_time,2)
-        time_mins = round(time_sec/60,2)
-        time_hours = round(time_sec/3600,2)
+        if input() == '':
+            print("Timer has stopped")
+            end_time=time.time()
 
-        if time_sec <= 60:
-            print("The time elapsed:",time_sec,'secs')
-            data.append(time_sec)
-            data.append('sec(s)')
-        elif 60 < time_sec <= 3600:
-            print("The time elapsed:",time_mins,'mins')
-            data.append(time_mins)
-            data.append('min(s)')
-        elif time_sec > 3600:
-            print("The time elapsed:",time_hours,'hours')
-            data.append(time_hours)
-            data.append('hour(s)')
-    #print(df)
-    break
+            time_sec = round(end_time-start_time,2)
+            time_mins = round(time_sec/60,2)
+            time_hours = round(time_sec/3600,2)
 
-#print(data)
-df1 = pd.DataFrame(columns=['DATE','Project name', 'Time', 'Unit']) 
-#writer = pd.ExcelWriter('Realized_hours.xlsx')
-df1_length = len(df1)
-df1.loc[df1_length] = data
-print(df1)
-#df.to_excel("Realized_hours.xlsx")
-df1.to_csv('Realized_hours.csv', mode='a', index=False, header=None)
+            if time_sec <= 60:
+                print("The time elapsed:",time_sec,'secs')
+                data.append(time_sec)
+                data.append('sec(s)')
+            elif 60 < time_sec <= 3600:
+                print("The time elapsed:",time_mins,'mins')
+                data.append(time_mins)
+                data.append('min(s)')
+            elif time_sec > 3600:
+                print("The time elapsed:",time_hours,'hours')
+                data.append(time_hours)
+                data.append('hour(s)')
+        #print(df)
+        break
 
-print('\n DONE. Press ENTER to exit.')
+    #print(data)
+    df1 = pd.DataFrame(columns=['DATE','Project name', 'Time', 'Unit']) 
+    #writer = pd.ExcelWriter('Realized_hours.xlsx')
+    df1_length = len(df1)
+    df1.loc[df1_length] = data
+    print(df1)
+    #df.to_excel("Realized_hours.xlsx")
+    df1.to_csv('Realized_hours.csv', mode='a', index=False, header=None)
+
+realized_h()
+
+print('\n DONE. Press ENTER to restart. Press X+ENTER to exit.')
 if input() == '':
+    realized_h()
+if input() == 'X'.casefold():
     sys.exit()
